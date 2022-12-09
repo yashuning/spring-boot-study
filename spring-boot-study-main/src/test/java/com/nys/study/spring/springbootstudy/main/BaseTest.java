5f34398a0d48b74f9d56611de3cf7be93bf7f1a6
ningyashu <ningyashu@meituan.com>
2022-12-09 16:02:48 +0800

    构建测试环境

---
+++ spring-boot-study-main/src/test/java/com/nys/study/spring/springbootstudy/main/BaseTest.java
@@ -0,0 +1,13 @@
+package com.nys.study.spring.springbootstudy.main;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author ningyashu
+ * @program: spring-boot-study
+ * @Description: 测试基类
+ * @date 2022/12/9 4:00 下午
+ */
+@SpringBootTest
+public class BaseTest {
+}
